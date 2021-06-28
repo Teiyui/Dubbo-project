bacd8f96c31e86d38bb54b68cb59f21a99e4fc13
Teiyui <Teiyui@outlook.com>
2021-06-28 09:20:24 +0800

    第三次提交，配置Dubbo参数timeout和retries

--- user-service-provider/src/main/java/com/zyw/service/impl/UserServiceImpl.java
+++ user-service-provider/src/main/java/com/zyw/service/impl/UserServiceImpl.java
@@ -15,10 +15,16 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     public List<UserAddress> getUserAddressList(String userId) {
+        System.out.println("UserServiceImpl.....1.....");
         UserAddress address1 = new UserAddress(1, "安徽省合肥市瑶海区教师新村A区二栋106", "1"
                 , "郑有为", "18940903056", "Y");
         UserAddress address2 = new UserAddress(2, "辽宁省大连市旅顺口区华发新城1栋1703", "1"
                 , "郑亦心", "13615518484", "N");
+        try {
+            Thread.sleep(4000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return Arrays.asList(address1, address2);
     }
